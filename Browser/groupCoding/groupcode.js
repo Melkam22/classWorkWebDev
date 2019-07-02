60c7b0682afe7af1676b9fd7f68be77d20650fa1
Ashenafi <asheaddis@yahoo.com>
2019-07-02 15:25:03 +0200

    the random number generation game

---
+++ Browser/groupCoding/groupcode.js
@@ -0,0 +1,36 @@
+
+ 
+ 
+
+/* function getRandomNumber( min, max){
+     let myRandom =   Math.floor(Math.random()*11);
+     min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max-min +1)+min); 
+}
+getRandomNumber(); */
+
+//random Number generation guess game
+ 
+let randomNumber =  Math.floor(Math.random()*11);
+
+console.log(randomNumber);
+function randomNumberGenerator(){
+    let guessedNumber = randomNumber;
+    let guessed=1;
+    guessed = Number(prompt("hi, please insert No. from 1 to 10"));
+    let myIncrement=1;
+
+    while(guessed !== guessedNumber && myIncrement <3){
+        guessed = prompt("what you guessed is wrong, note that you have only 2 more attempts left")
+        myIncrement++;
+    } 
+     if( guessed === guessedNumber && myIncrement <3){
+        alert("Bravo! you guessed it right!")
+        //myIncrement++;
+    } else {
+         alert("sorry, you have failed to get it right") 
+    }
+   
+}
+randomNumberGenerator();
