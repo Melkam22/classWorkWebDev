595e4b36c1082b8e15acd11fee6831f4a11c8ed8
Ashenafi <asheaddis@yahoo.com>
2019-07-03 10:32:01 +0200

    add randomNumber generator

---
+++ Browser/groupCoding/guessing-game2/guessgame2.js
@@ -0,0 +1,11 @@
+
+let randomNumber = Math.floor(Math.random()*11);
+console.log(randomNumber);
+
+function randomNumberGame2(){
+    let playersGuess = randomNumber;
+    let guess =1;
+    let myIncrement =1;
+    //guess = Number(prompt("put any number b/n 1 & 10 here"));
+}
+randomNumberGame2();
