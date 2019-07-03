272c2d693c1d3f487ec199413ffe2999718168aa
Ashenafi <asheaddis@yahoo.com>
2019-07-03 19:46:20 +0200

    add postpresentation push

--- Browser/groupCoding/guessing-game2/guessgame2.js
+++ Browser/groupCoding/guessing-game2/guessgame2.js
@@ -9,26 +9,29 @@ return Math.floor(Math.random()*(max-min +1))+min;
 let submitBtn = document.querySelector("#btn");
 submitBtn.addEventListener("click", guessedNumber)
 
-    let randomizedNumber = randomNumber(1,10);
+    let randomizedNumber = randomNumber(50,60);
     let myIncrement=0;
      
 function guessedNumber(){
     myIncrement++;
-    console.log(myIncrement);
+    //console.log(myIncrement);
     let inputValue = Number(document.querySelector("input").value);
-    console.log(randomizedNumber);
-    console.log(inputValue);
+    //console.log(randomizedNumber);
+    //console.log(inputValue);
     let heading = document.querySelector("h1");
     let paragraph = document.querySelector("p");
 
     if(randomizedNumber !== inputValue && myIncrement <3){
         heading.innerHTML="Wrong!!!";
-        paragraph.innerHTML=`Try again! You have ${3-myIncrement} attempt${3-myIncrement==1? "": "s"} left`;
+        paragraph.innerHTML=`Try again! You have ${3-myIncrement} 
+        attempt${3-myIncrement<=1? "": "s"} left`;
          
     }
  else if (randomizedNumber === inputValue && myIncrement <=3){
         heading.innerHTML="Bravo!!!";
-        paragraph.innerHTML=`The number is indeed ${randomizedNumber}, you got the number correctly after ${3-myIncrement} attempt${myIncrement==1? "": "s"}`;
+        paragraph.innerHTML=`The number is indeed ${randomizedNumber}, 
+        you got the number correctly after ${3-myIncrement} 
+        attempt${3-myIncrement<=1? "": "s"}`;
     }  
     else {
         heading.innerHTML="Sorry, you lost!";
