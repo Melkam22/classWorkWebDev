7a842cf0942da9f1c489545fe7e406f2e7637353
Ashenafi <asheaddis@yahoo.com>
2019-07-03 11:09:25 +0200

    change randomNumber generator

--- Browser/groupCoding/guessing-game2/guessgame2.js
+++ Browser/groupCoding/guessing-game2/guessgame2.js
@@ -1,11 +1,16 @@
 
-let randomNumber = Math.floor(Math.random()*11);
-console.log(randomNumber);
 
-function randomNumberGame2(){
+function randomNumber(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+return Math.floor(Math.random()*(max-min +1))+min;
+}
+
+
+/* function randomNumberGame2(){
     let playersGuess = randomNumber;
     let guess =1;
     let myIncrement =1;
     //guess = Number(prompt("put any number b/n 1 & 10 here"));
 }
-randomNumberGame2();
+randomNumberGame2(); */
