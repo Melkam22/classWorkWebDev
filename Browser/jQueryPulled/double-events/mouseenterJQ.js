01440006314bdcc79723f21da03681f56cad4e8e
Ashenafi <asheaddis@yahoo.com>
2019-07-04 14:39:33 +0200

    add enter&leave events final

--- Browser/jQueryPulled/double-events/mouseenterJQ.js
+++ Browser/jQueryPulled/double-events/mouseenterJQ.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
         if(event.type === 'mouseenter') {
         $('#btn').css('color', 'red');
         $('#btn').css('background-color', 'black');
-        $("#btn").html("This is my first text");
+        $("#btn").html("Don't leave me this way!");
         $("#btn").css("border-radius", "10px");
-        } else{
+        } else if (event.type === 'mouseenter') {
             $('#btn').css('color', 'tomato');
             $('#btn').css('background-color', 'silver');
             $("#btn").html("This is my second text");
+        } else {
+            $("#btn").css("color", "orange");
+            $("#btn").css("background-color", "rgba(11,20,25,.5");
+            $("#btn").html("Just put the cursor back on me!");
             $("#btn").css("border-radius", "0px");
         }
     })
