c967e8964df37167ce55a22a5589ce150be13332
Ashenafi <asheaddis@yahoo.com>
2019-07-04 11:52:20 +0200

    add jquery istyling ntro

---
+++ Browser/Jquery/jquery.js
@@ -0,0 +1,23 @@
+
+$("h1").html("from the jquery");
+
+$(document).ready(function() {
+    $('#clc').click(function() {  
+        $('.para').html('This is my paragraph, fired from jquery.');
+        $(".para").css("color", "red");
+    })
+})
+//to style all together
+
+$("#clc").click(function(){
+    $(".para").css({
+        "background-color" : "red",
+        "border" : "1px solid black",
+        "width" : "40%",
+        "height" : "40vh",
+        "background-color" : "rgba(12,143,198,.5)",
+        "margin-left" : "25%",
+    })
+})       
+
+ 
