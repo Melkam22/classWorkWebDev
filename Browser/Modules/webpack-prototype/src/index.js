8459dde2c449eb813a464b43d7a3d1d72a2102a0
Ashenafi <asheaddis@yahoo.com>
2019-07-16 14:39:29 +0200

    add final push, bundled using webpack

--- Browser/Modules/webpack-prototype/src/index.js
+++ Browser/Modules/webpack-prototype/src/index.js
@@ -1,7 +1,61 @@
 
-/* import createBanner from "./banner.js";
+/* import createBanner from "./banner.js";*/
 
-import WebpackImg from "./webpack-logo.png";
+//the button active effect
+let myInput = document.querySelector("input");
+myInput.addEventListener("focus", function(event){
+    event.target.style.backgroundColor="black";
+    event.target.style.color="green";
+},true);
+
+myInput.addEventListener("blur", function(event){
+    event.target.style.backgroundColor="";
+},true);
+
+//fetching data
+let myIcon = document.querySelector(".icon");
+let myTemperature = document.querySelector(".temperature");
+let myWind = document.querySelector(".wind");
+let myCountry = document.querySelector(".country");
+let myHumidity = document.querySelector(".humidity");
+let myImg =document.querySelector("#img");
+let myButton = document.querySelector("#btn");
+
+myButton.addEventListener("click", function(){
+    let myCity = document.querySelector("#cityName").value;
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=${myCity}&APPID=950a99d07777436d293fd763f07f0f90&units=metric`;
+
+    fetch(api)
+    .then(response=> {
+        return response.json()
+    })
+    .then(data=>{
+        console.log(data)
+
+        document.querySelector(".span").innerHTML += data.weather[0].description;
+        myImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;//for icons
+        myTemperature.innerHTML += data.main.temp + " °C";
+        myWind.innerHTML += data.wind.speed;
+        myCountry.innerHTML += data.sys.country;
+        myHumidity.innerHTML += data.main.humidity+"%";
+    })
+})
+
+let myExtra = document.querySelector(".extraBox");
+    let myDate = new Date();
+myExtra.innerHTML= myDate.toString();
+
+ 
+
+
+
+
+
+
+
+
+
+/*import WebpackImg from "./webpack-logo.png";
 
 const createElement = message => {
   const element = document.createElement("div");
