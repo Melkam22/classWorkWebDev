23155ff422b3a83d04b44ccbdb5f42eacfd26d95
Ashenafi <asheaddis@yahoo.com>
2019-08-08 15:58:19 +0200

    final comment

--- Browser/ashenafi-to-do-list/src/components/ToD0Form.jsx
+++ Browser/ashenafi-to-do-list/src/components/ToD0Form.jsx
@@ -1,22 +1,53 @@
 import React , {Component} from 'react';
 
 class ToDoForm extends Component {
-    handleSubmit(e){
-        e.preventDefault();
-        console.log('Create an event');
+    
+    state = {
+        inputText: null,
+        todoItems: [],/* to create a dummylist, sort of database, or leave them empty*/
     }
-    render(){
-        return (
+//this.handleSubmit = this.handleSubmit.bind(this);
+
+handleInputChange = event => {
+        const inputText = event.target.value;
+        this.setState({inputText: inputText})
+        console.log("inputText:",inputText);
+    }
+    
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        console.log(this);
+
+        this.setState(prevState=>({
+            todoItems: [...prevState.todoItems, this.state.inputText]
+        }))
+    }
+
+render(){
+return (
+    <React.Fragment> 
             <form className='input-group my-3'
             onSubmit={this.handleSubmit}>
   
-    <input type="text" className="form-control" placeholder="Add a new to do item"/>
+    <input /* type="text" */ className="form-control"
+    name="name"
+    type="text" 
+    placeholder="Add a new to do item..."
+    onChange={this.handleInputChange} />
    
-    <input type="password" className="form-control" placeholder="Password"/>
-    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-  <button type="submit" className="btn btn-primary">Submit</button>
+    <div className="input-group-append"> 
+    
+    <button className="btn btn-outline-secondary" type="submit">
+    <i className="fas fa-plus" aria-hidden="true" />&nbsp;Add item
+    </button>
+     </div>
 </form>
-        )
-    }
+{/* shortcircuit */}
+{this.state.inputText && <p>Typing: {this.state.inputText}</p>}
+{this.state.todoItems.map((items, index)=><li key={index}>{items}</li>)}
+{/* we ll have our false database on our page */}
+</React.Fragment>
+    );
+}
 }
 export default ToDoForm
