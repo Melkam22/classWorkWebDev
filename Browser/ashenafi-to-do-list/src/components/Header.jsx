bde35dbb9982be8f3399a38587156cd2184793a9
Ashenafi <asheaddis@yahoo.com>
2019-08-08 15:58:08 +0200

    final comment

--- Browser/ashenafi-to-do-list/src/components/Header.jsx
+++ Browser/ashenafi-to-do-list/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React  from 'react'
 import './Header.css'
+import logo from '../logo.svg';
 const Header = props=> {
         return (
             <div className='header'>
                 <h1>What to do?</h1>
                <div>{props.tagline}</div>
+               <img src={logo} alt="logo" width="150"/>
             </div>
         )
     }
