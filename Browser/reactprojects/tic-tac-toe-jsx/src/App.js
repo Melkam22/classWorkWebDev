aff8d4261f1553967a4dd4370803ecabf2e0c8a2
Ashenafi <asheaddis@yahoo.com>
2019-08-14 10:59:21 +0200

    all works correctly except the reset button

--- Browser/reactprojects/tic-tac-toe-jsx/src/App.js
+++ Browser/reactprojects/tic-tac-toe-jsx/src/App.js
@@ -3,14 +3,14 @@ import Photo from './photo.jpg';
 import './App.css';
 import Main from './components/main.jsx';
 
-const Example = (props) =>{
+/* const Example = (props) =>{
   console.log(props.age)
   return("Hi")
-}
+} */
 
 class App extends Component {
 
-  winningLines = [
+  /* winningLines = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -20,20 +20,21 @@ class App extends Component {
     [0, 4, 8],
     [2, 4, 6],
   ];
-
+ */
   render(){
   return (
     <>
       <h1>This is Tic-Tac-Toe with React-jsx</h1> 
       <img src={Photo} alt="Photo" className="photo" />
 
-      <Main 
+    <Main />
+      {/* <Main 
         banana={this.winningLines} 
         age="12"
         name="Lol"
         calcAge={()=>{console.log("hi")}}
       />
-      <Example age={12}/>
+      <Example age={12}/> */}
     </>
   );
 }
--- Browser/reactprojects/tic-tac-toe-jsx/src/components/main.jsx
+++ Browser/reactprojects/tic-tac-toe-jsx/src/components/main.jsx
@@ -1,18 +1,15 @@
 import React, {Component} from 'react';
 //import './App.js';
 
-class Main extends Component {
-    
+class Main extends Component {  
 state = {
     title: "Tic-Tac-Toe",
-    myVariable: new Array(9).fill(null),
-    y: "O",
+    gameStatus: new Array(9).fill(null),
+    player: "X" ? "O" : "X",//if X the next-one is O
 }
-// change the function name to be more declarative
-addItem=(e, i)=>{
+
+clickedSelection=(e, i)=>{
     e.stopPropagation()
-    // 1. Get current player. X? O?
-    // 2. Go to the game status, update the status with the current click/selection
     // 2. a) check if the item clicked is already clicked (check if null)
     //      - if it is clicked do nothing
     //      - if it is not clicked change it to whatever current player is (x or o)
@@ -20,59 +17,64 @@ addItem=(e, i)=>{
 
     // pass tempArray inside the calculateWinner function (not the this.state.gameState)
 
-
-
-    // let tempArray = this.state.gameStatus
-    // tempArray[i] = this.state.currentPlayer
-    // null => "X" (if the current player is X)
-    // this.setState({gameStatus: tempArray})
-
-
-    let newVariable=this.state.myVariable;
-    if(this.state.myVariable[i] === null){
-    newVariable[i]=this.state.myPlayer
-    let player = this.state.myPlayer === "X" ? "O" : "X";
-    this.calculateWinner(player);
+//gameStatus & player updated 
+    let currentGameStatus=this.state.gameStatus;
+    if(this.state.gameStatus[i] === null){
+    currentGameStatus[i]=this.state.player
+    let currentPlayer = this.state.player === "X" ? "O" : "X";
+    this.calculateWinner(currentGameStatus);//currentPlayer
     this.setState({
-        myVariable:newVariable,
-        myPlayer:player   
-       })
+        gameStatus:currentGameStatus,
+        player:currentPlayer   
+       });
     }
 }
  
-calculateWinner(myPlayer) {
+calculateWinner(currentGameStatus) {
+    let winningLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
 
-    // you want to read the gameStatus from the state
-    // put the winningLines in the state?
-    // set state something like : gameWinner : null
-    // player X O draw
-
-// for(let o=0; o < winningLines.length; o ++ ){
-//     const [a, b, c] = winningLines[o];
-// if(myPlayer[a] && myPlayer[a] === myPlayer[b] && myPlayer[a] === myPlayer[c]){
-//     //return myPlayer[a];
-//     alert("you are the winner!");
-// }
-// } 
+for(let d=0; d < winningLines.length; d ++ ){
+const [a, b, c] = winningLines[d];
+if(currentGameStatus[a] && currentGameStatus[a] === currentGameStatus[b] && currentGameStatus[a] === currentGameStatus[c]){
+   let score = document.querySelector(".result").innerHTML += "Bravo! you won.";
+}
+else if(currentGameStatus[a] && currentGameStatus[a] !== currentGameStatus[b] && currentGameStatus[a] !== currentGameStatus[c]){
+    //let score = document.querySelector(".result").innerHTML += "draw, reset & play again.";
+}
+ } 
 //return null;
 }
 
+//reset button
+function () {
+    let object = this.onResetClick();
+    if (object) {
+      //console.log(value);
+      this.setState({object: null}); // <-- reset value
+    }
+  }
+
 
 render(){
-
     console.log(this.props);
-    
-
 return(
-    
     <>
     <h1>Tic-Tac-Toe</h1>
     <div className="result">Result:</div>
-    <button className="reset">Reset</button>
+    <button className="reset" onClick={this.onResetClick}>Reset</button>
 
     <div id="container">
-    {this.state.myVariable.map((item, i)=>{
-        return (<div onClick={(e)=>this.addItem(e, i)} className="boxes" key={i}><span className="circles">{item}</span></div>)
+    {this.state.gameStatus.map((item, i)=>{
+        return (<div onClick={(e)=>this.clickedSelection(e, i)} className="boxes" key={i}><span className="circles">{item}</span></div>)
     })}
     </div>
      </>
